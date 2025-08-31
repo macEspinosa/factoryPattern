eb7531009c7477b6de7a0639731fc91983705011
macEspinosa <mariaantonetteespinosa26@gmail.com>
2025-08-31 18:28:32 +0800

    Update Clinic.java

--- factoryPattern/Clinic.java
+++ factoryPattern/Clinic.java
@@ -4,36 +4,43 @@ import java.util.Scanner;
 
 public class Clinic {
     public static void main(String[] args){
+       Scanner scanner = new Scanner(System.in);
 
-        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("\nChoose a pet:");
+            System.out.println("1. Cat");
+            System.out.println("2. Dog");
+            System.out.println("3. Exit");
+            System.out.print("Enter choice: ");
 
-        System.out.println("[1] Dog");
-        System.out.println("[2] Cat");
-        System.out.print("\nChoose your pet number: ");
-        Integer choice = input.nextInt();
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // consume newline
 
-        PetRecord petFile = new PetRecord();
-        Pet pet;
+            if (choice == 1) {
+                Cat cat = new Cat(9); // cats have 9 lives
+                System.out.println("Cat selected.");
+                System.out.println("Number of lives: " + cat.noofLives());
+                System.out.println("Sound: " + cat.makeSound());
+                System.out.println("Play: " + cat.play());
 
-        switch(choice){
-            case 1: pet = new Dog();
-                petFile.setPetId("D01");
-                petFile.setPetName("Bantay");
-                petFile.setPet(pet);
-                ((Dog) pet).setBreed("German Shepperd");
+            } else if (choice == 2) {
+                System.out.print("Enter dog breed: ");
+                String breed = scanner.nextLine();
+                Dog dog = new Dog(breed);
+                System.out.println("Dog selected.");
+                System.out.println("Breed: " + dog.getBreed());
+                System.out.println("Sound: " + dog.makeSound());
+                System.out.println("Play: " + dog.play());
+
+            } else if (choice == 3) {
+                System.out.println("Exiting program...");
                 break;
-            case 2: pet = new Cat();
-                petFile.setPetId("C01");
-                petFile.setPetName("Muning");
-                petFile.setPet(pet);
-                ((Cat) pet).setNoOfLives(9);
+
+            } else {
+                System.out.println("Invalid choice! Please select 1, 2, or 3.");
+            }
         }
 
-        System.out.println("Pet id is " + petFile.getPetId());
-        System.out.println("Pet name is " + petFile.getPetName());
-        System.out.println("Pet kind: " + petFile.getPet().getClass().getSimpleName());
-        System.out.println("Communication sound: "+ petFile.getPet().makeSound());
-        System.out.println("Play mode: " + petFile.getPet().play());
-
+        scanner.close();
     }
 }
